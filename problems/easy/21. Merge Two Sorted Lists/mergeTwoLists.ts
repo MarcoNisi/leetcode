0e8fe659154d4f1c195f7b0796e629b02b722f0e
Marco Nisi <marco.nisi.100@gmail.com>
2022-03-29 18:02:34 +0200

    Merge two sorted lists problem

---
+++ problems/easy/21. Merge Two Sorted Lists/mergeTwoLists.ts
@@ -0,0 +1,53 @@
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+Constraints:
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+*/
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+const mergeTwoLists = (list1: ListNode | null, list2: ListNode | null): ListNode | null => {
+  if (list1 === null && list2 === null) {
+    return null
+  } else if (list1 === null) {
+    return list2
+  } else if (list2 === null) {
+    return list1
+  } else {
+    if (list1.val < list2.val) {
+      return {
+        val: list1.val,
+        next: mergeTwoLists(list1.next, list2)
+      }
+    } else {
+      return {
+        val: list2.val,
+        next: mergeTwoLists(list2.next, list1)
+      }
+    }
+  }
+}
