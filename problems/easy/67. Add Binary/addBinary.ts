a2a12b3867825bdc8ac8e98c508c067f6997ad77
Marco Nisi <marco.nisi.100@gmail.com>
2022-04-13 18:27:45 +0200

    Refresh of Add Binary problem

--- problems/easy/67. Add Binary/addBinary.ts
+++ problems/easy/67. Add Binary/addBinary.ts
@@ -15,25 +15,29 @@ a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
 */
 const addBinary = (a: string, b: string): string => {
+  let carry = 0
   let result = ''
   let i = a.length - 1
   let j = b.length - 1
-  let carry = 0
   while (i >= 0 || j >= 0) {
     let sum = carry
     if (i >= 0) {
       sum += Number(a[i])
-      i--
     }
     if (j >= 0) {
       sum += Number(b[j])
-      j--
     }
+    i--
+    j--
     result = sum % 2 + result
     carry = Math.floor(sum / 2)
   }
-  if (carry > 0) {
-    result = 1 + result
+  if (carry) {
+    result = carry + result
   }
   return result
 }
+
+console.log(addBinary('1010', '1011'))
+console.log(addBinary('11', '1'))
+console.log(addBinary('1', '0'))
