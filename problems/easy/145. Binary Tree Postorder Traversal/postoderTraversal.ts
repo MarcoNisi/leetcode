9a303023a0941936aab90e1f2ad06ce0c660cfdd
Marco Nisi <marco.nisi.100@gmail.com>
2022-08-18 15:52:21 +0200

    Binary Tree Postorder Traversal problem

--- problems/easy/145. Binary Tree Postorder Traversal/postoderTraversal.ts
+++ problems/easy/145. Binary Tree Postorder Traversal/postoderTraversal.ts
@@ -1,9 +1,9 @@
 /*
-Given the root of a binary tree, return the preorder traversal of its nodes' values.
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
 Example 1:
 Input: root = [1,null,2,3]
-Output: [1,2,3]
+Output: [3,2,1]
 
 Example 2:
 Input: root = []
@@ -14,7 +14,7 @@ Input: root = [1]
 Output: [1]
 
 Constraints:
-The number of nodes in the tree is in the range [0, 100].
+The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
@@ -30,23 +30,23 @@ class TreeNode {
   }
 }
 
-const preorderTraversal = (root: TreeNode | null): number[] => {
+const postorderTraversal = (root: TreeNode | null): number[] => {
   if (!root) return []
-  const queue: (TreeNode | null)[] = [root]
+  const queue: TreeNode[] = [root]
   const values: number[] = []
   while (queue.length) {
     const element = queue.shift()
     if (element) {
-      values.push(element.val)
-      if (element.right) {
-        queue.unshift(element.right)
-      }
       if (element.left) {
         queue.unshift(element.left)
       }
+      if (element.right)  {
+        queue.unshift(element.right)
+      }
+      values.unshift(element.val)
     }
   }
   return values
 }
 
-export default preorderTraversal
+export default postorderTraversal
